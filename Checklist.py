e917cf1f11a51dccdbeaa4656f31b5642b00de07
mandatamanvendra <manvendrasinghkhangarot062@gmail.com>
2026-08-21 13:06:49 +0530

    Add QA learning roadmap with checkboxes

--- Checklist.py
+++ Checklist.py
@@ -1,1 +1,52 @@
+import Streamlit as st
+st.title("QA learning roadmap tracking")
+
+st.header("python")
+
+st.checkbox("variable")
+st.checkbox("data types")
+st.checkbox("if/else")
+st.checkbox("while loops")
+st.checkbox("for loops")
+st.checkbox("functions")
+st.checkbox("lists")
+st.checkbox("calculator project")
+st.checkbox("file handling")
+st.checkbox("modules")
+st.checkbox("try/except")
+
+st.header("SQL")
+
+st.checkbox("create database")
+st.checkbox("create table")
+st.checkbox("insert")
+st.checkbox("select")
+st.checkbox("where")
+st.checkbox("order by")
+st.checkbox("update")
+st.checkbox("delete")
+st.checkbox("count")
+st.checkbox("sum")
+st.checkbox("AVG")
+st.checkbox("join")
+
+st.header("Manual testing ")
+
+st.checkbox("Test case")
+st.checkbox("bug reporting ")
+st.checkbox("severity vs priority")
+st.checkbox("bug life cycle")
+st.checkbox("jira basics")
+
+st.header("automation Testing")
+
+st.checkbox("selenium")
+st.checkbox("playwright")
+st.checkbox("locators ")
+st.checkbox("automation project")
+
+st.success("keep learning daily")
+
+
+
 
